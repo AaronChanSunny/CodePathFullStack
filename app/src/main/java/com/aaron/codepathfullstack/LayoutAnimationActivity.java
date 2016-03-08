1ed6aa4d46cd2e050efc3b9c4b7b31df591f7ed0
aaronchansunny <aaronchansunny@gmail.com>
2016-03-08 22:16:22 +0800

    animating changes

--- app/src/main/java/com/aaron/codepathfullstack/LayoutAnimationActivity.java
+++ app/src/main/java/com/aaron/codepathfullstack/LayoutAnimationActivity.java
@@ -5,9 +5,19 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LayoutAnimationActivity extends AppCompatActivity {
+
+    private List<String> mDatas;
+    private ArrayAdapter<String> mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +35,39 @@ public class LayoutAnimationActivity extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        initDatas();
+
+        mAdapter = new ArrayAdapter<>(this, android.R.layout
+                .simple_list_item_1, android.R.id.text1, mDatas);
+        ListView listView = (ListView) findViewById(R.id.list_view);
+        listView.setAdapter(mAdapter);
     }
 
+    private void initDatas() {
+        mDatas = new ArrayList<>();
+
+        for (int i = 0; i < 5; i++) {
+            mDatas.add("Item " + i);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_layout_animation, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.action_add:
+                mDatas.add("Item " + mDatas.size());
+                mAdapter.notifyDataSetChanged();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
