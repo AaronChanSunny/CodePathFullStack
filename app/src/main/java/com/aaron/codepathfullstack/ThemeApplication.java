e4a08e9ef51e52822a088140234aadc4f73356af
AaronChan <aaronchansuuny@gmail.com>
2016-03-09 17:51:22 +0800

    complete Theme.Material_Light

---
+++ app/src/main/java/com/aaron/codepathfullstack/ThemeApplication.java
@@ -0,0 +1,11 @@
+package com.aaron.codepathfullstack;
+
+import android.app.Application;
+
+/**
+ * Created by Aaron on 2016/3/9.
+ */
+public class ThemeApplication extends Application {
+
+    public static int currentPosition;
+}
